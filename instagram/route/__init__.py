8a66b3be0086ec75df171e725a1722e4d7df3ae5
Taras Panechko <cch.taras.panechko@cleanconnect.ai>
2025-09-22 17:35:32 +0300

    removed procedures

--- instagram/route/__init__.py
+++ instagram/route/__init__.py
@@ -4,5 +4,5 @@ from instagram.controller.user_controller import user_bp
 
 def init_routes(app):
     app.register_blueprint(user_bp, url_prefix='/app')
-    app.register_blueprint(comment_bp, url_prefix='/procedures')
+    # app.register_blueprint(comment_bp, url_prefix='/procedures')
 
